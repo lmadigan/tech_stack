a7a25adf2775be5009423a884a888e1775eb214e
lauren.madigan <lauren.madigan@workday.com>
2018-03-12 20:59:17 -0700

    Add ListView to list on screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import combineReducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <Provider store={ createStore(combineReducers) }>
       <View>
        <Header headerText="Tech Stack" />
+       <LibraryList />
       </View>
     </Provider>
   );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
-  return() {
-    return;
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow() {
+    
+  }
+
+  render() {
+    return(
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
-export default connect()(LibraryList);
- 
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
+
+
+// this.props.libraries.map( lib => {
+//     return (
+//       <View>
+//         <ListItem>lib.name</ListItem>
+//         </View>
+//     );
+// });
