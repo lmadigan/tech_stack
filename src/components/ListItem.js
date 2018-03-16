c76833dfad5df6a55bd3c66f0f80174294c57473
lauren.madigan <lauren.madigan@workday.com>
2018-03-16 09:44:51 -0700

    final techstack with Layout animation and TouchableWithoutFeedback

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    //called whenever the component is about to be rerendered to the screen
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
 
   render() {
     const { titleStyle } = styles;
@@ -18,6 +33,7 @@ class ListItem extends Component {
           <CardSection>
             <Text style={titleStyle}>{this.props.library.title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -31,7 +47,12 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded: expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
 //automatic binding with connect function - don't need to use dispatch anymore
 
 //connect can be used to call an action creator
